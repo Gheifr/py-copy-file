755b5f69d1c4137d38748bcfc44aa899e9735682
Vitalii Prushak <v.prushak@gmail.com>
2025-04-17 20:49:27 +0300

    Solution: attempt #4

--- app/main.py
+++ app/main.py
@@ -13,12 +13,12 @@ def copy_file(command: str) -> None:
 
     if exists(source_file):
         if exists(target_file):
-            with (open(target_file, "r") as target,
-                  open(source_file, "r") as source):
-                if samefile(target.name, source.name):
-                    return
+            if samefile(target_file, source_file):
+                return
 
         with (open(source_file, "r") as source,
               open(target_file, "w") as target):
             for line in source:
-                target.writelines(line)
+                target.write(line)
+        return
+    return

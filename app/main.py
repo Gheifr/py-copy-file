4277fe3b638eecd6414e42cb512def2f2e3c3696
Vitalii Prushak <v.prushak@gmail.com>
2025-04-17 20:13:28 +0300

    Solution: attempt #1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from os.path import exists
+
+
+def copy_file(command: str) -> None:
+    args = command.split(" ")
+    if len(args) != 3:
+        return
+    if args[0] != "cp":
+        return
+
+    source_file = args[1]
+    target_file = args[2]
+
+    if not source_file == target_file and exists(source_file):
+        with (open(target_file, "w") as target,
+              open(source_file, "r") as source):
+            try:
+                target.writelines(source.readlines())
+            except FileNotFoundError:
+                raise
